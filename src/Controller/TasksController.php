b17bf40cf6b78c2c86b346027958a0fe5f39eb94
Aksenchenko Stephan <s.aksenchenko@mygento.ru>
2022-07-20 14:54:16 +0300

    On branch main 	Add tasks add form

--- src/Controller/TasksController.php
+++ src/Controller/TasksController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use TaskType;
 
 class TasksController extends AbstractController
 {
@@ -15,6 +18,28 @@ class TasksController extends AbstractController
             'controller_name' => 'TasksController',
         ]);
     }
+
+    #[Route('/tasks/add', name: 'app_tasks_add')]
+    public function add(Request $request): Response
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $task = $form->getData();
+
+            return $this->redirectToRoute('app_tasks__add_successfully');
+        }
+
+        
+
+        return $this->renderForm('tasks/add.html.twig', [
+            'form' => $form
+        ]);
+    }
+
     #[Route('/tasks/add/successfully', name: 'app_tasks__add_successfully')]
     public function addedSuccessfully()
     {
---
+++ src/Form/Type/TaskType.php
@@ -0,0 +1,17 @@
+<?php
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('save', SubmitType::class)
+        ;
+    }
+}
