e6c2d0fcf75129c3175e06a34be8d20d1d463da4
Aksenchenko Stephan <s.aksenchenko@mygento.ru>
2022-07-20 15:14:53 +0300

    On branch main 	Add new field to task

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\TaskRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
@@ -15,6 +16,9 @@ class Task
 
     #[ORM\Column(length: 255)]
     private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
 
     public function getId(): ?int
     {
@@ -29,6 +33,18 @@ class Task
     public function setTitle(string $title): self
     {
         $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
